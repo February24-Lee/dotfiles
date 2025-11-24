1f3c56f45ce64fa421e9761cd88d9a1731e45077
February24-Lee <hiiamdong@gmail.com>
2025-11-24 17:14:44 +0900

    config: integrate tmux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -144,6 +144,24 @@ require("lazy").setup({
         "sindrets/diffview.nvim"        -- optional - Diff Integration
         }
     },
+    {
+      "christoomey/vim-tmux-navigator",
+      cmd = {
+        "TmuxNavigateLeft",
+        "TmuxNavigateDown",
+        "TmuxNavigateUp",
+        "TmuxNavigateRight",
+        "TmuxNavigatePrevious",
+        "TmuxNavigatorProcessList",
+      },
+      keys = {
+        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+        { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+      },
+    },
 })
 
 -- 🌟 Mason & LSP auto setup
